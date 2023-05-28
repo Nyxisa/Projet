aaf1b325581b5a53dbe5af031899ff393772d8a5
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-05-28 20:48:56 +0200

    à propos en cours

--- Projet_Groupe3/src/pages/about.vue
+++ Projet_Groupe3/src/pages/about.vue
@@ -1,4 +1,43 @@
 <template>
-
-    
+    <div class="">
+        <div class="relative">
+            <img src="/img/title-pic.webp" alt="épis de blé" class="w-full">
+            <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+                <h1 class="text-2xl font-bold text-darkgreen">La Cagette Fermière&nbsp;? Qui sommes-nous&nbsp;?</h1>
+            </div>
+        </div>
+        <section class="p-[7vw]">
+            <img src="/img/about-confiture.webp" alt="pots de confiture">
+            <h2 class="pt-6 text-3xl font-bold lg:text-6xl">À propos de La Cagette Fermière</h2>
+            <p class="font-serif text-2xl font-bold lg:text-xl text-darkgreen">Vous en avez assez du gaspillage
+                alimentaire&nbsp;?<br>Nous aussi&nbsp;!</p>
+            <p class="pt-2 text-sm lg:text-xl text-darkgreen">C'est pourquoi nous avons créé La Cagette fermière, une
+                plateforme
+                de vente de fruits et légumes aux formes authentiques et originales.</p>
+            <p class="pt-2 text-sm lg:text-xl text-darkgreen"> Nous travaillons en étroite collaboration avec des
+                agriculteurs et primeurs locaux afin de récupérer leurs invendus.
+                Cela nous permet de vous proposer des produits frais de qualité, moins chers qu'en grande
+                surface&nbsp;!</p>
+        </section>
+        <section class="px-[7vw]">
+            <img src="/img/about-fraises.webp" alt="plat rempli de fraises">
+            <h2 class="pt-6 text-3xl font-bold lg:text-6xl">Notre histoire</h2>
+            <p class="font-serif text-2xl font-bold lg:text-xl text-darkgreen">Étudiantes en But MMI & écolo</p>
+            <p class="pt-2 text-sm lg:text-xl text-darkgreen">Nous sommes des étudiantes en BUT Métiers du Multimédia et de
+                l'internet.
+                L'écologie et la lutte contre le gaspillage sont des valeurs qui nous tiennent à cœur.</p>
+            <p class="pt-2 text-sm lg:text-xl text-darkgreen">C'est pourquoi, dans le cadre d'un exercice pédagogique, nous
+                avons pris l'initiative de mettre en place un site afin de limiter le gaspillage alimentaire.<br>
+                Et ce, tout en offrant la possibilité aux habitants du Lot et Garonne d'économiser sur les fruits, les
+                légumes et les produits du jardin.</p>
+        </section>
+        <section class="p-[7vw]">
+            <h2 class="text-3xl font-bold lg:text-6xl">Les membres de l'équipe</h2>
+            <div class="h-[250px] w-[250px] bg-lightgreen rounded-full"></div>
+            <div class="flex items-center mt-4">
+        <p class="mr-4">Votre texte ici</p>
+        <img src="/img/lisa.webp" alt="Description de l'image" class="w-10 h-10">
+      </div>
+        </section>
+    </div>
 </template>
