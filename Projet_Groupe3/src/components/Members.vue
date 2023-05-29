039ef1790560fceb3dc582debbf5cd332406ee0f
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-05-29 18:50:28 +0200

    ajout d'image et section membres ok

--- Projet_Groupe3/src/components/Members.vue
+++ Projet_Groupe3/src/components/Members.vue
@@ -2,8 +2,47 @@
 import linkedin from '@/components/icons/linkedin.vue';
 </script>
 <template>
-    <div class="relative p-4 m-auto">
-        <img src="/img/lisa-mobile.webp" alt="Avatar de Lisa">
+    <div class="flex flex-col flex-wrap justify-center gap-10 my-10 md:flex-row">
+        <div class="relative flex flex-col items-center md:w-[450px]">
+            <div class="w-[300px] h-[300px] bg-brown rounded-full"></div>
+            <div class="absolute z-10 mt-16 mr-44 md:mr-28">
+                <div>
+                    <h2 class="text-2xl font-bold text-right text-white">Lisa</h2>
+                    <p class="mt-4 text-sm text-right text-white max-w-[100px]">Manageuse du groupe</p>
+                    <a href="#" class="flex justify-end mt-4">
+                        <linkedin />
+                    </a>
+                </div>
+            </div>
+            <img src="/img/lisa2.webp" alt="Image" class="absolute top-0 right-0 w-56">
+        </div>
+
+        <div class="relative flex flex-col items-center md:w-[450px]">
+            <div class="w-[300px] h-[300px] bg-darkgreen rounded-full"></div>
+            <div class="absolute z-10 mt-16 ml-44 md:ml-28">
+                <div>
+                    <h2 class="text-2xl font-bold text-white">Julia</h2>
+                    <p class="mt-4 text-sm text-white max-w-[100px]">Chargée de relation avec nos partenaires</p>
+                    <a href="#" class="flex justify-start mt-4">
+                        <linkedin />
+                    </a>
+                </div>
+            </div>
+            <img src="/img/julia2.webp" alt="Image" class="absolute top-0 left-0 w-56">
+        </div>
+
+        <div class="relative flex flex-col items-center md:w-[450px]">
+            <div class="w-[300px] h-[300px] bg-lightgreen rounded-full"></div>
+            <div class="absolute z-10 mt-16 mr-44 md:mr-28">
+                <div>
+                    <h2 class="text-2xl font-bold text-right text-white">Jaël</h2>
+                    <p class="mt-4 text-sm text-right text-white max-w-[100px]">Chargée de communication</p>
+                    <a href="#" class="flex justify-end mt-4">
+                        <linkedin />
+                    </a>
+                </div>
+            </div>
+            <img src="/img/jael2.webp" alt="Image" class="absolute top-0 right-0 w-56">
+        </div>
     </div>
-    <linkedin />
 </template>
--- Projet_Groupe3/src/pages/about.vue
+++ Projet_Groupe3/src/pages/about.vue
@@ -14,10 +14,10 @@ import Members from '@/components/Members.vue'
         <h2 class="pt-6 text-3xl font-bold lg:text-6xl">À propos de La Cagette Fermière</h2>
         <p class="font-serif text-2xl font-bold lg:text-xl text-darkgreen">Vous en avez assez du gaspillage
             alimentaire&nbsp;?<br>Nous aussi&nbsp;!</p>
-        <p class="pt-2 text-sm lg:text-xl text-darkgreen">C'est pourquoi nous avons créé La Cagette fermière, une
+        <p class="pt-2 text-sm text-black lg:text-xl">C'est pourquoi nous avons créé La Cagette fermière, une
             plateforme
             de vente de fruits et légumes aux formes authentiques et originales.</p>
-        <p class="pt-2 text-sm lg:text-xl text-darkgreen"> Nous travaillons en étroite collaboration avec des
+        <p class="pt-2 text-sm text-black lg:text-xl"> Nous travaillons en étroite collaboration avec des
             agriculteurs et primeurs locaux afin de récupérer leurs invendus.
             Cela nous permet de vous proposer des produits frais de qualité, moins chers qu'en grande
             surface&nbsp;!</p>
@@ -26,10 +26,10 @@ import Members from '@/components/Members.vue'
         <img src="/img/about-fraises.webp" alt="plat rempli de fraises">
         <h2 class="pt-6 text-3xl font-bold lg:text-6xl">Notre histoire</h2>
         <p class="font-serif text-2xl font-bold lg:text-xl text-darkgreen">Étudiantes en But MMI & écolo</p>
-        <p class="pt-2 text-sm lg:text-xl text-darkgreen">Nous sommes des étudiantes en BUT Métiers du Multimédia et de
+        <p class="pt-2 text-sm text-black lg:text-xl">Nous sommes des étudiantes en BUT Métiers du Multimédia et de
             l'internet.
             L'écologie et la lutte contre le gaspillage sont des valeurs qui nous tiennent à cœur.</p>
-        <p class="pt-2 text-sm lg:text-xl text-darkgreen">C'est pourquoi, dans le cadre d'un exercice pédagogique, nous
+        <p class="pt-2 text-sm text-black lg:text-xl">C'est pourquoi, dans le cadre d'un exercice pédagogique, nous
             avons pris l'initiative de mettre en place un site afin de limiter le gaspillage alimentaire.<br>
             Et ce, tout en offrant la possibilité aux habitants du Lot et Garonne d'économiser sur les fruits, les
             légumes et les produits du jardin.</p>
