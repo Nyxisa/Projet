cb5202b8204a0bf076753f3f6e167baf5be841f8
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-06-03 23:04:26 +0200

    page 404

--- Projet_Groupe3/src/main.ts
+++ Projet_Groupe3/src/main.ts
@@ -1,18 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import routes from '~pages'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router'
 import { createHead } from 'unhead'
 import V3ScrollLock from 'v3-scroll-lock'
+import NotFound from '@/components/Page404.vue';
 
 import './assets/css/style.css'
 
 const app = createApp(App)
 
-
 createHead()
 
-const scrollToAnchor = (to) => {
+const scrollToAnchor = (to: RouteLocationNormalized) => {
   if (to.hash) {
     const element = document.getElementById(to.hash.slice(1));
     if (element) {
@@ -23,17 +23,21 @@ const scrollToAnchor = (to) => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
-
+app.use(V3ScrollLock, {})
 app.use(
   createRouter({
     history: createWebHistory(),
-    routes,
+    routes: [
+    ...routes,
+    {
+      path: '/:catchAll(.*)', 
+      component: NotFound,
+    },],
     scrollBehavior(to, from, savedPosition) {
     scrollToAnchor(to);
   },
   })
 )
 
-app.use(V3ScrollLock, {})
 
 app.mount('#app')
