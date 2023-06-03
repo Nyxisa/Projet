4e424d5e4eb60a2988965280ca4e2f3667539433
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-06-03 19:09:02 +0200

    fix footer

--- Projet_Groupe3/src/components/FooterPage.vue
+++ Projet_Groupe3/src/components/FooterPage.vue
@@ -7,12 +7,12 @@ import instagram from '@/components/icons/instagram.vue'
 
 <template>
     <footer>
-        <div class="w-full gap-5 py-8 bg-brown md:grid">
-            <div class="flex flex-row justify-center col-span-2 col-start-2 gap-4 md:flex-col md:gap-0">
+        <div class="justify-center gap-8 py-8 md:px-8 lg:grille bg-brown md:flex">
+            <div class="flex flex-row justify-center col-span-3 col-start-1 gap-4 md:flex-col md:gap-0">
                 <RouterLink to="/" class="block">
                     <Logo />
                 </RouterLink>
-                <div class="gap-4 md:flex-col md:flex">
+                <div class="gap-4 lg:flex-col lg:flex">
                     <div>
                         <ul>
                             <li class="font-sans text-sm text-white">6 rue du château</li>
@@ -31,28 +31,28 @@ import instagram from '@/components/icons/instagram.vue'
                 </div>
             </div>
 
-            <nav class="my-6 md:grid md:col-span-2 md:col-start-4">
+            <nav class="col-span-2 col-start-4 my-6">
                 <h4 class="font-serif text-2xl text-center text-white md:text-left">La cagette fermière</h4>
                 <RouterLink to="/products" class="footer-link">Nos produits</RouterLink>
                 <RouterLink to="/abonnements" class="footer-link">Nos cagettes</RouterLink>
                 <RouterLink to="/dons" class="footer-link">Faire un don de produits</RouterLink>
             </nav>
 
-            <nav class="my-6 md:grid md:col-span-2 md:col-start-6">
+            <nav class="col-span-2 col-start-6 my-6">
                 <h4 class="font-serif text-2xl text-center text-white md:text-left">À propos</h4>
                 <RouterLink to="/about" class="footer-link">Qui sommes-nous</RouterLink>
                 <RouterLink to="/partenaires" class="footer-link">Nos partenaires</RouterLink>
                 <RouterLink to="/productors" class="footer-link">Nos producteurs</RouterLink>
             </nav>
 
-            <nav class="my-6 md:grid md:col-span-2 md:col-start-8">
+            <nav class="col-span-2 col-start-8 my-6">
                 <h4 class="font-serif text-2xl text-center text-white md:text-left">Informations</h4>
                 <RouterLink to="/legals#1" class="footer-link">Politique de confidentalité</RouterLink>
                 <RouterLink to="/legals#2" class="footer-link">Utilisation des cookies</RouterLink>
                 <RouterLink to="/legals#3" class="footer-link">Mentions légales</RouterLink>
             </nav>
 
-            <div class="my-6 md:grid md:col-span-2 md:col-start-10">
+            <div class="col-span-3 col-start-10 my-6">
                 <h4 class="font-serif text-2xl text-center text-white md:text-left">Nous contacter</h4>
                 <RouterLink to="/contact" class="footer-link">Formulaire de contact</RouterLink>
                 <a href="tel:0769408355" class="footer-link">07 69 40 83 55</a>
