6589237ce86867a779a431484c40464c332467f7
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-06-04 21:08:56 +0200

    création page panier et compte

---
+++ Projet_Groupe3/src/pages/compte.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="relative">
+        <img src="/img/title-pic.webp" alt="épis de blé" class="w-full">
+        <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+            <h1 class="top-banner">Votre Compte</h1>
+        </div>
+    </div>
+</template>
---
+++ Projet_Groupe3/src/pages/panier.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="relative">
+        <img src="/img/title-pic.webp" alt="épis de blé" class="w-full">
+        <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+            <h1 class="top-banner">Vous avez 0 articles dans votre panier</h1>
+        </div>
+    </div>
+</template>
