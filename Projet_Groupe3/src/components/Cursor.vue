c883a8f9c15ef5ef1580546609b4e2156ede51c8
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-06-05 14:10:31 +0200

    curseur ok

---
+++ Projet_Groupe3/src/components/Cursor.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="slider-component">
+            <div class="slidecontainer">
+                <input v-model="currentValue" type="range" :min="min" :max="max" class="slider" @input="onInput">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        value: {
+            type: Number,
+            required: true
+        },
+        min: {
+            type: Number,
+            required: true
+        },
+        max: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {
+            currentValue: this.value
+        };
+    },
+    methods: {
+        onInput() {
+            // Convertir la valeur en string
+            const valueAsString = this.currentValue.toString();
+            // Émettre l'événement avec la valeur convertie en nombre
+            this.$emit('input', parseInt(valueAsString));
+        }
+
+    }
+});
+</script>
---
+++ Projet_Groupe3/src/pages/sauver.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Cursor from '@/components/Cursor.vue'
+</script>
+
+<template>
+    <div class="relative">
+        <img src="/img/title-pic.webp" alt="Fruits et légumes décoratifs" class="w-full">
+        <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+            <h1 class="top-banner">Comment fonctionnent les dons ?</h1>
+        </div>
+    </div>
+    <Cursor :value="10" :min="0" :max="20"/>
+     
+</template>
+
