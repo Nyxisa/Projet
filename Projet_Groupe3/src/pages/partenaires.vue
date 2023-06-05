3358bd9cf1956cd58d22fcf98a8e1997200e9da5
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-06-05 08:30:07 +0200

    partenaires ok

--- Projet_Groupe3/src/pages/partenaires.vue
+++ Projet_Groupe3/src/pages/partenaires.vue
@@ -3,5 +3,22 @@
 </script>
 
 <template>
-    
+    <div class="relative">
+        <img src="/img/title-pic.webp" alt="épis de blé" class="w-full">
+        <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+            <h1 class="top-banner">Nos partenaires</h1>
+        </div>
+    </div>
+    <div class="px-[8vw] py-6">
+        <h2 class="mb-4 leading-tight">Nous sommes fiers de travailler en collaboration avec des éco-partenaires</h2>
+        <p>Ils nous soutiennent, nous financent et croient en nos valeurs.</p>
+        <div class="justify-around gap-8 p-8 lg:flex lg:my-10">
+            <img class="mx-auto my-8 lg:m-0 lg:h-[100px] xl:h-[150px]" src="/img/partenaire-1.webp"
+                alt="Lot et Garonne, le département coeur du Sud-Ouest">
+            <img class="mx-auto my-8 lg:m-0 lg:h-[100px] xl:h-[150px]" src="/img/partenaire-2.webp"
+                alt="Tomates Concombres, l'association nationale de producteurs">
+            <img class="mx-auto my-8 lg:m-0 lg:h-[100px] xl:h-[150px]" src="/img/partenaire-3.webp"
+                alt="Union nationale du commerce de gros de fruits et légumes">
+        </div>
+    </div>
 </template>
