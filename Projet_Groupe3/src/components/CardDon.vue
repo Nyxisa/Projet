3b744797c16ebad5abf0be13995bcf051559e060
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-06-05 13:16:28 +0200

    page dons ok

--- Projet_Groupe3/src/components/CardDon.vue
+++ Projet_Groupe3/src/components/CardDon.vue
@@ -5,20 +5,22 @@ import Message from './icons/message.vue';
 </script>
 
 <template>
-    <div class="flex flex-col items-center overflow-hidden bg-white rounded-xl shadow-1 gap-4 m-[5vw] max-w-[250px]">
-        <p class="font-serif text-2xl font-bold text-center text-white lg:text-3xl"></p>
-        <div class="flex flex-col items-start self-stretch bg-white rounded-xl px-[2vh] gap-4">
-            <img src="/img/don.png" alt="imgAlt" class="self-stretch object-cover rounded-xl" />
+    <div class="block">
+        <div class="flex flex-col items-center overflow-hidden bg-white rounded-xl shadow-1 m-[5vw] w-[250px]  lg:m-0">
+            <p class="font-serif text-2xl font-bold text-center text-white lg:text-3xl"></p>
+            <div class="flex flex-col items-start self-stretch bg-white rounded-xl px-[2vh] gap-4">
+                <img src="/img/don.png" alt="imgAlt" class="self-stretch object-cover rounded-xl" />
 
 
-            <div class="px-[1vh] relative flex items-center self-stretch justify-between">
-                <p class="mb-0 text-base break-words lg:text-3xl">Cerises</p>
-                <Message class="w-[36px]" />
-            </div>
-            <div class="pb-2 px-[1vh]">
-                <p class="text-sm text-left break-words lg:text-base">Cueillette trop abondante pour moi, je donne deux
-                    paniers...</p>
-                <p class="text-sm lg:text-base">Distance : 5km</p>
+                <div class="px-[1vh] relative flex items-center self-stretch justify-between my-2">
+                    <p class="mb-0 text-base break-words lg:text-3xl">Cerises</p>
+                    <Message class="w-[36px]" />
+                </div>
+                <div class="pb-2 px-[1vh]">
+                    <p class="text-sm text-left break-words lg:text-base">Cueillette trop abondante pour moi, je donne deux
+                        paniers...</p>
+                    <p class="text-sm lg:text-base">Distance : 5km</p>
+                </div>
             </div>
         </div>
     </div>
--- Projet_Groupe3/src/pages/dons.vue
+++ Projet_Groupe3/src/pages/dons.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CardDon from '@/components/CardDon.vue';
+import CardProductor from '@/components/CardProductor.vue';
 
 
 </script>
@@ -8,8 +9,16 @@ import CardDon from '@/components/CardDon.vue';
     <div class="relative">
         <img src="/img/title-pic.webp" alt="Fruits et légumes décoratifs" class="w-full">
         <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-            <h1 class="top-banner">Faire un don</h1>
+            <h1 class="top-banner">Comment fonctionnent les dons ?</h1>
         </div>
+    </div>
+    <div class="flex w-full">
+        <RouterLink to="/"
+            class="w-1/2 px-4 py-2 text-center text-white uppercase bg-lightgreen lg:mt-0 active:border-lightgreen hover:drop-shadow-lg active:border-4 lg:text-xl">
+            Donner</RouterLink>
+        <RouterLink to="/"
+            class="w-1/2 px-4 py-2 text-center text-white uppercase bg-orange lg:mt-0 active:border-orange hover:drop-shadow-lg active:border-4 lg:text-xl">
+            Sauver</RouterLink>
     </div>
     <div class="flex flex-col justify-center gap-4 mx-4 my-8">
         <img src="/img/illu-eco.webp" alt="panier de légumes" class="max-w-[54px] m-auto">
@@ -23,34 +32,54 @@ import CardDon from '@/components/CardDon.vue';
             <span class="uppercase text-orange">DIFFORMES</span>.
         </p>
         <h3 class="text-3xl font-bold text-center text-darkgreen lg:text-5xl">Mais toujours aussi savoureux&nbsp;!</h3>
-        <RouterLink to="/" class="m-auto my-8 btn">
+        <RouterLink to="/" class="mx-auto my-8 btn">
             FAIRE UN DON</RouterLink>
     </div>
 
-    <section class="px-[8vw] py-12">
+    <section class="px-[8vw]">
 
         <h5>Donner des produits</h5>
-        <h3 class="text-darkgreen">Vous êtes un particulier&nbsp;?</h3>
-        <p>Découvrez les étapes à suivre pour faire des dons de produits grâce à La Cagette Fermière.</p>
-        <p>Chez La Cagette Fermière, la solidarité et le partage sont des valeurs fortes.</p>
-        <p>C’est pourquoi faire un don vous permet de bénéficiez de 20% de réduction sur votre panier lors de votre première
-            commande.</p>
-        <p>Rejoignez notre communauté engagée et faites profiter de nombreuses personnes de vos récoltes&nbsp;!</p>
-        <RouterLink to="/" class="m-auto my-8 btn">Je veux donner</RouterLink>
+        <h3 class="mb-2 text-darkgreen">Vous êtes un particulier&nbsp;?</h3>
+        <div class="items-start gap-[4vw] lg:flex">
+            <div>
+                <p class="pt-2">Découvrez les étapes à suivre pour faire des dons de produits grâce à La Cagette Fermière.</p>
+                <p>Chez La Cagette Fermière, la solidarité et le partage sont des valeurs fortes.</p>
+                <p>C'est pourquoi faire un don vous permet de bénéficiez de 20% de réduction sur votre panier lors de votre
+                    première
+                    commande.</p>
+                <p class="mb-6">Rejoignez notre communauté engagée et faites profiter de nombreuses personnes de vos
+                    récoltes&nbsp;!
+                </p>
+                <RouterLink to="/" class="block mx-auto my-8 btn w-fit lg:mx-0">Je veux donner</RouterLink>
+            </div>
+                <div class="flex py-6 space-x-2 overflow-x-auto lg:gap-12 lg:py-0 lg:w-[30vw] lg:overflow-visible">
+                    <CardDon class="max-w-[250px] w-full" />
+                    <CardDon class="lg:hidden" />
+                    <CardDon class="lg:hidden" />
+                </div>
+            
+        </div>
     </section>
     <section class="px-[8vw] py-12">
 
+
         <h5>Devenir partenaire</h5>
-        <h3 class="text-darkgreen">Vous êtes Agriculteur, Fermier, Producteur...?</h3>
-        <p>Si vous êtes agriculteur dans le Lot et Garonne vous pouvez devenir partenaire avec la Cagette Fermière&nbsp;!
-        </p>
-        <p>Pour ce faire, il vous suffira de remplir un formulaire que vous pourrez télécharger ici.</p>
-        <p>Comme Jade et Maxime ou encore Christophe, faites le choix de donner une seconde vie à vos invendus tout en
-            profitant d’une rémunération supplémentaire.</p>
-
-
-        <RouterLink to="/" class="m-auto my-8 btn">Rejoignez-nous</RouterLink>
+        <h3 class="mb-2 text-darkgreen">Vous êtes Agriculteur, Fermier, Producteur...?</h3>
+        <div class="items-start gap-8 lg:flex">
+            <div>
+                <p class="pt-2">Si vous êtes agriculteur dans le Lot et Garonne vous pouvez devenir partenaire avec la
+                    Cagette Fermière&nbsp;!
+                </p>
+                <p>Pour ce faire, il vous suffira de remplir un formulaire que vous pourrez télécharger ici.</p>
+                <p>Comme Jade et Maxime ou encore Christophe, faites le choix de donner une seconde vie à vos invendus tout
+                    en
+                    profitant d'une rémunération supplémentaire.</p>
+                <RouterLink to="/" class="block mx-auto my-8 btn w-fit lg:mx-0">Rejoignez-nous</RouterLink>
+            </div>
+            <div class="flex justify-around gap-[2vw] py-0">
+                <CardProductor class="max-w-[250px] w-full" />
+                <CardProductor class="max-w-[250px] w-full" />
+            </div>
+        </div>
     </section>
-
-    <CardDon/>
-    </template>
+</template>
